68c9d07e1db5764f8dedb8f2ed169f08abccca4d
IAm5High <745170149@qq.com>
2019-04-23 18:04:55 +0800

    添加like组件点击事件

--- components/like/index.js
+++ components/like/index.js
@@ -4,20 +4,37 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+      like: {
+        type: Boolean
+      },
+      count: {
+        type: Number,
+      }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+      
+      like_image_src: 'images/喜欢@2x.png',
+      dislike_image_src: 'images/喜欢dis@2x.png'
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+      onLike: function(event){
+        let like = this.properties.like
+        let count = this.properties.count
 
+        count = like ? count - 1 : count + 1
+
+        this.setData({
+          count: count,
+          like: !like
+        })
+      }
   }
 })
